0d25d640b54428b57ce7128da1e8c803810192cd
bahirithikaramp <bahirithi.karampudi@gmail.com>
2022-10-06 22:54:23 +0530

    Final App

---
+++ src/DigitButton.js
@@ -0,0 +1,5 @@
+import { ACTIONS } from "./App"
+
+export default function DigitButton({ dispatch, digit }) {
+    return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload:{digit} })}>{digit}</button>
+}
---
+++ src/OperationButton.js
@@ -0,0 +1,5 @@
+import { ACTIONS } from "./App"
+
+export default function OperationButton({ dispatch, operation }) {
+    return <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload:{operation} })}>{operation}</button>
+}
